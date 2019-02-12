0e039910f6ba1bfe27f271c290991ee60c30dcaf
Felipe Lucio <felipeluc@gmail.com>
2019-02-12 21:10:49 -0200

    fixed navbar auto collapse

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
+global.JQuery = require('jquery');
+require('popper.js');
+require('bootstrap');
+
 import React from 'react';
 import { render } from 'react-dom';
 
 import App from './app';
 
 function main(ev) {
-    //let app = new App(elems);
-    //window._app = app;
     render(<App />, document.getElementById('app'));
-
 }
 
 window.addEventListener('load', main);
--- src/js/nav_bar.jsx
+++ src/js/nav_bar.jsx
@@ -3,26 +3,33 @@ import React from 'react';
 export default class NavBar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Basic</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Eletronic</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Isotopes</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Radius</a>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        <select onChange={(e) => this.props.langOnChange(e.target.value)}>
-          <option value="en">English</option>
-          <option value="pt-BR">Português</option>
-        </select>
+        <div  className="navbar-collapse collapse" id="navbar-content">
+          <ul className="navbar-nav ">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Basic</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Eletronic</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Isotopes</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Radius</a>
+            </li>
+          </ul>
+          <form className="ml-auto form-inline">
+            <select className="form-control" onChange={(e) => this.props.langOnChange(e.target.value)}>
+              <option value="en">English</option>
+              <option value="pt-BR">Português</option>
+            </select>
+          </form>
+        </div>
       </nav>
     );
   }
