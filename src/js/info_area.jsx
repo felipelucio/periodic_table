5df0a47cbd47a75c61ad2695d7916393df8ee0fd
Felipe Lucio <felipeluc@gmail.com>
2019-03-01 10:37:53 -0300

    added info_area elementbox

--- src/js/info_area.jsx
+++ src/js/info_area.jsx
@@ -9,15 +9,32 @@ export default class InfoArea extends React.Component {
   }
 
   render() {
+    let lang = this.props.lang;
     let elem = this.props.element;
 
+    if(!elem) return (<li className="info_area"></li>);
+    
     return (
-      <li className="info_area"> 
-      {elem && 
+      <li className={`info_area`}> 
         <span className={`basic ${this._showFlags('basic')}`}>
-          <p>{elem.symbol}</p>
+          <span className={`elementbox  ${elem.type}`}>
+            <span className="symbol">{elem.symbol}</span>
+            <span className="name">{elem.getName(lang)}</span>
+            <span className="atomic_number">{elem.atomic_number}</span>
+            <span className="weight">
+              {elem.weight && elem.weight.hasOwnProperty('conventional') &&
+                <div>{`[${elem.weight.standard[0]},${elem.weight.standard[1]}]`}</div>
+              }
+              <div>{elem.getWeight(false)}</div>
+            </span>
+          </span>
+          <span className="info">
+            <span className="">blablabalbal</span>
+            <span className="">blablabalbal</span>
+            <span className="">blablabalbal</span>
+            <span className="">blablabalbal</span>
+          </span>
         </span>
-      }
       </li>
     );
   }
